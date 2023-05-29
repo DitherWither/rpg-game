6ada8189bd21ab123f735810cb3c62fb50a529cf
DitherWither <demonslayervardhan@gmail.com>
2023-05-29 16:19:38 +0530

    Add layers

--- src/Map.cpp
+++ src/Map.cpp
@@ -6,8 +6,10 @@
 
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <iostream>
 
 using json = nlohmann::json;
+
 
 auto read_file(std::string_view path) -> std::string {
     constexpr auto read_size = std::size_t(4096);
@@ -33,23 +35,32 @@ Map::Map(Tileset *overworldTileset, Tileset *caveTileset, Tileset *innerTileset,
     std::string fileString = read_file(filename);
     json fileJson = json::parse(fileString);
 
-    this->mapContents = fileJson["tiles"].get<std::vector<std::vector<int>>>();
-    this->mapStyles = fileJson["style"].get<std::vector<std::vector<std::string>>>();
+    std::vector<json> layersJson = fileJson["layers"].get<std::vector<json>>();
+
+    for (auto layerJson: layersJson) {
+        MapLayer layer;
+
+        layer.tiles = layerJson["tiles"].get<std::vector<std::vector<int>>>();
+        layer.style = layerJson["style"].get<std::vector<std::vector<std::string>>>();
+        this->layers.push_back(layer);
+    }
 }
 
+
 void Map::draw() {
-    for (int column = 0; column < this->mapContents.size(); column++) {
-        for (int row = 0; row < this->mapContents[column].size(); row++) {
-            int xpos = row * this->overworldTileset->tileWidth;
-            int ypos = column * this->overworldTileset->tileHeight;
+    for (auto layer: this->layers) {
+        for (int column = 0; column < layer.tiles.size(); column++) {
+            for (int row = 0; row < layer.tiles[column].size(); row++) {
+                int xpos = row * this->overworldTileset->tileWidth;
+                int ypos = column * this->overworldTileset->tileHeight;
+                if (layer.style[column][row] == "o")
+                    overworldTileset->draw(layer.tiles[column][row], xpos, ypos);
+                else if (layer.style[column][row] == "c")
+                    caveTileset->draw(layer.tiles[column][row], xpos, ypos);
+                else if (layer.style[column][row] == "i")
+                    innerTileset->draw(layer.tiles[column][row], xpos, ypos);
 
-            if (mapStyles[column][row] == "o")
-                overworldTileset->draw(mapContents[column][row], xpos, ypos);
-            else if (mapStyles[column][row] == "c")
-                caveTileset->draw(mapContents[column][row], xpos, ypos);
-            else if(mapStyles[column][row] == "i")
-                innerTileset->draw(mapContents[column][row], xpos, ypos);
-
+            }
         }
     }
 }
